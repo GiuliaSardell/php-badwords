ad8e6aa0005850119c1bcb4bbd09a8e226c73551
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-12-17 16:22:47 +0100

    stampa paragrafo

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$text= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus commodi, aspernatur repudiandae nihil laudantium molestiae voluptatum dicta perferendis eveniet similique!'
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<h2> <?php echo $text ?> </h2>
+
+</body>
+</html>
